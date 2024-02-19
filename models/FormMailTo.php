b314af9d22a5433889eeee3b38aba400cb09a44c
user <sallamyemen@gmail.com>
2024-02-19 18:38:28 +0300

    mobile corrections

---
+++ models/FormMailTo.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class FormMailTo extends Model
+{
+    public $firstName;
+    public $mail;
+    public $phone;
+    public $comment;
+
+    public function rules()
+    {
+        return [
+            [['firstName', 'mail', 'phone', 'comment'], 'safe'],
+        ];
+    }
+}
--- views/site/_section-form.php
+++ views/site/_section-form.php
@@ -1,5 +1,6 @@
 <?php
-
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 ?>
 <div class="form" id="form">
     <div class="form__title">
@@ -11,17 +12,35 @@
         </div>
     </div>
     <div class="form__wrap">
-        <form action="mailto:anna@kolambassador.com" method="post" enctype="text/plain">
-            <input type="hidden" name="subject" value="Presentation order">
-            <input type="hidden" name="body" value="Hello my name is %name%! %0A %0A %message%. %0A %0A You can contact me by email: %email% or phone: %phone% please?">
-            <input type="text" name="name" class="form__control" placeholder="Your Name" required><br>
-            <input type="text" name="email" class="form__control" placeholder="Your Email" required><br>
-            <input type="text" name="phone" class="form__control" placeholder="Your Phone" required><br>
-            <textarea name="message" class="form__control" placeholder="Your Message" required></textarea><br>
-            <input type="submit" value="Send">
-        </form>
+<!--        <form action="mailto:anna@kolambassador.com" method="get" enctype="text/plain">-->
+<!--            <input type="hidden" name="subject" value="Presentation order">-->
+<!--            <input type="hidden" name="body" value="Hello my name is %name%! %0A %0A %message%. %0A %0A You can contact me by email: %email% or phone: %phone% please?">-->
+<!--            <input type="text" name="name" class="form__control" placeholder="Your Name" required><br>-->
+<!--            <input type="text" name="email" class="form__control" placeholder="Your Email" required><br>-->
+<!--            <input type="text" name="phone" class="form__control" placeholder="Your Phone" required><br>-->
+<!--            <textarea name="message" class="form__control" placeholder="Your Message" required></textarea><br>-->
+<!--            <input type="submit" value="Send">-->
+<!--        </form>-->
+        <?php $form = ActiveForm::begin([
+            'id' => 'uploadForm',
+            'options' => ['enctype' => 'multipart/form-data']
+        ]) ?>
+
+        <?= $form->field($model, 'firstName')->textInput(['placeholder' => 'First name', 'class' => 'form__control'])->label(false)?>
+        <?= $form->field($model, 'mail')->textInput(['placeholder' => 'E-mail', 'class' => 'form__control'])->label(false) ?>
+        <?= $form->field($model, 'phone')->textInput(['placeholder' => 'phone', 'class' => 'form__control', 'id' => 'model-phone'])->label(false) ?>
+        <?= $form->field($model, 'comment')->textInput(['placeholder' => 'Message', 'class' => 'form__control'])->label(false) ?>
+        <?= Html::submitButton('Send', ['class' => 'form__submit', 'name' => 'submit-button']) ?>
+
+        <?php ActiveForm::end() ?>
         <div class="form__h2">
             <?= Yii::t('app', 'We’ll try to answer as quickly as possible') ?>
         </div>
     </div>
 </div>
+<?php
+$js = <<<JS
+$('#model-phone').mask('999-999-9999');
+JS;
+$this->registerJs($js);
+?>
